3ed37ad9fd86ded1875a42562a0f2bcec32be3b0
Ramonprog <programador.ramon@gmail.com>
2023-12-17 19:22:08 -0300

    ajustando logica do timer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Form from './components/Form/Form'
 import NavBar from './components/NavBar/NavBar'
 import { AllTimes } from './types';
 import { Progress } from 'antd';
+import _ from 'lodash';
 
 function App() {
 
@@ -13,6 +14,10 @@ function App() {
     training: 0,
     rest: 0
   });
+  const [initialTime, setInitialTime] = useState<AllTimes>({
+    training: 0,
+    rest: 0
+  })
 
   const [cycle, setCycle] = useState(0)
 
@@ -26,26 +31,35 @@ function App() {
       ...alltimes,
       [type]: time.$s
     });
+    setInitialTime({
+      ...alltimes,
+      [type]: time.$s
+    });
   }
 
   const [isActive, setIsActive] = useState(false);
+  const [progress, setProgress] = useState((alltimes.training / initialTime.training) * 100)
+
 
   useEffect(() => {
     let interval;
-    const initialTime = alltimes
 
+    const updateProgress = () => {
+
+      const porcentagem = (alltimes.training / initialTime.training) * 100;
+      setProgress(porcentagem);
+
+    };
 
     if (isActive) {
       interval = setInterval(() => {
         setAllTimes((prevAllTimes) => {
           if (prevAllTimes.training > 0 && prevAllTimes.rest !== 0) {
-            console.log(prevAllTimes.training, 'step 1');
             return {
               ...prevAllTimes,
               training: prevAllTimes.training - 1,
             };
           } else if (prevAllTimes.training === 0 && prevAllTimes.rest > 0) {
-            console.log(prevAllTimes.rest, 'step 2');
             return {
               ...prevAllTimes,
               rest: prevAllTimes.rest - 1,
@@ -53,34 +67,30 @@ function App() {
           } else if (prevAllTimes.rest === 0 && prevAllTimes.training === 0) {
             if (cycle > 0) {
               setCycle((prev) => prev - 1);
-              console.log(cycle, 'step 3');
               return {
                 training: initialTime.training,
                 rest: initialTime.rest,
               };
             } else {
               setIsActive(false);
-              console.log(cycle, 'step 4');
               return initialTime;
             }
           }
-          return prevAllTimes;
         });
-
+        updateProgress();
       }, 1000);
     }
 
-
     return () => clearInterval(interval);
-  }, [isActive, cycle]);
 
+  }, [isActive, cycle, alltimes.training]);
 
 
   return (
     <>
       <NavBar />
       <Form func={handleTimeChange} setCycle={setCycle} handleSubmit={handleSubmit} />
-      <Progress format={(percent) => percent + 's'} type="circle" percent={alltimes.training}
+      <Progress format={() => alltimes.training + "s"} type="circle" percent={progress + 1}
         status="active" strokeColor={conicColors} />
     </>
   )
